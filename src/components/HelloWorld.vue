b171fcffed20d2b845941233b6ede67afc8100bc
connor.stroomberg <connorstroomberg@users.noreply.github.com>
2017-12-20 22:09:33 +0100

    minimal test for molgenis-ui-form component

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <form-component></form-component>
+  </div>
+</template>
+
+<script>
+import FormComponent from '@molgenis/molgenis-ui-form'
+
+export default {
+  name: 'HelloWorld',
+  components: {
+    FormComponent
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
